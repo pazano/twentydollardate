ba7b6917e99a67f56d2427efc568d3fb1b1e7486
Paul Valenzano <paul.valenzano@gmail.com>
2017-02-04 15:39:25 -0800

    http canonical links for single pages, fb comments fix?

--- web/app/themes/twentydollar/header.php
+++ web/app/themes/twentydollar/header.php
@@ -10,25 +10,35 @@
 	<title><?php wp_title( '-', true, 'right' ); ?></title>
 
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
-	
+
+	<?php
+		if (is_single()) :
+			$http_permalink = str_replace('https://', 'http://', get_permalink());
+	?>
+			<link rel="canonical" href="<?php echo($http_permalink); ?>" />
+	<?php
+		endif;
+	?>
+
+
 	<?php if(get_theme_mod('sp_favicon')) : ?>
 	<link rel="shortcut icon" href="<?php echo get_theme_mod('sp_favicon'); ?>" />
 	<?php endif; ?>
-	
+
 	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
 	<link rel="alternate" type="application/atom+xml" title="<?php bloginfo('name'); ?> Atom Feed" href="<?php bloginfo('atom_url'); ?>" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-	
+
 	<?php wp_head(); ?>
-	
+
 </head>
 
 <body <?php body_class(); ?>>
 
 	<div id="top-bar">
-	
+
 		<div class="container">
-			
+
 		<div id="logo">
 			<h1><a href="<?php echo home_url(); ?>">Home</a></h1>
 		</div>
@@ -36,26 +46,26 @@
 			<div id="nav-wrapper">
 				<?php wp_nav_menu( array( 'container' => false, 'theme_location' => 'main-menu', 'menu_class' => 'menu' ) ); ?>
 			</div>
-			
+
 			<div class="menu-mobile"></div>
-			
+
 			<?php if(!get_theme_mod('sp_topbar_search_check')) : ?>
 			<div id="top-search">
 				<?php get_search_form(); ?>
 				<i class="fa fa-search search-desktop"></i>
 				<i class="fa fa-search search-toggle"></i>
 			</div>
-			
+
 			<!-- Responsive Search -->
 			<div class="show-search">
 				<?php get_search_form(); ?>
 			</div>
 			<!-- -->
 			<?php endif; ?>
-			
+
 			<?php if(!get_theme_mod('sp_topbar_social_check')) : ?>
 			<div id="top-social" <?php if(get_theme_mod('sp_topbar_search_check')) : ?>class="nosearch"<?php endif; ?>>
-				
+
 				<?php if(get_theme_mod('sp_facebook')) : ?><a href="http://facebook.com/<?php echo esc_html(get_theme_mod('sp_facebook')); ?>" target="_blank"><i class="fa fa-facebook"></i></a><?php endif; ?>
 				<?php if(get_theme_mod('sp_twitter')) : ?><a href="http://twitter.com/<?php echo esc_html(get_theme_mod('sp_twitter')); ?>" target="_blank"><i class="fa fa-twitter"></i></a><?php endif; ?>
 				<?php if(get_theme_mod('sp_instagram')) : ?><a href="http://instagram.com/<?php echo esc_html(get_theme_mod('sp_instagram')); ?>" target="_blank"><i class="fa fa-instagram"></i></a><?php endif; ?>
@@ -69,41 +79,41 @@
 				<?php if(get_theme_mod('sp_vimeo')) : ?><a href="http://vimeo.com/<?php echo esc_html(get_theme_mod('sp_vimeo')); ?>" target="_blank"><i class="fa fa-vimeo-square"></i></a><?php endif; ?>
 				<?php if(get_theme_mod('sp_linkedin')) : ?><a href="<?php echo esc_html(get_theme_mod('sp_linkedin')); ?>" target="_blank"><i class="fa fa-linkedin"></i></a><?php endif; ?>
 				<?php if(get_theme_mod('sp_rss')) : ?><a href="<?php echo esc_url(get_theme_mod('sp_rss')); ?>" target="_blank"><i class="fa fa-rss"></i></a><?php endif; ?>
-				
+
 			</div>
 			<?php endif; ?>
-			
+
 		</div>
-	
+
 	</div>
 
 
 	<header id="header">
-	
+
 		<div class="container">
 <!--
 			<div id="logo">
-				
+
 				<?php if(!get_theme_mod('sp_logo')) : ?>
-					
+
 					<?php if(is_front_page()) : ?>
 						<h1><a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="<?php bloginfo( 'name' ); ?>" /></a></h1>
 					<?php else : ?>
 						<h2><a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="<?php bloginfo( 'name' ); ?>" /></a></h2>
 					<?php endif; ?>
-					
+
 				<?php else : ?>
-					
+
 					<?php if(is_front_page()) : ?>
 						<h1><a href="<?php echo home_url(); ?>"><img src="<?php echo esc_url(get_theme_mod('sp_logo')); ?>" alt="<?php bloginfo( 'name' ); ?>" /></a></h1>
 					<?php else : ?>
 						<h2><a href="<?php echo home_url(); ?>"><img src="<?php echo esc_url(get_theme_mod('sp_logo')); ?>" alt="<?php bloginfo( 'name' ); ?>" /></a></h2>
 					<?php endif; ?>
-					
+
 				<?php endif; ?>
-				
+
 			</div>
 -->
 		</div>
-		
+
 	</header>
